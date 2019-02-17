d7eb1d0030559662825aed30eff7d72dbe50ec12
Kyle McCaffery <mccaffk2@wwu.edu>
2019-02-17 02:21:23 -0800

    Cleaned up code

--- options.js
+++ options.js
@@ -12,45 +12,32 @@ function saveOptions(e) {
   function restoreOptions() {
   
     function setCurrentChoice1(result) {
-      document.querySelector("#URL1").value = result.URL1 || "https://redsquarewebcam.wwu.edu/mjpg/video.mjpg";
+        document.querySelector("#URL1").value = result.URL1 || "https://redsquarewebcam.wwu.edu/mjpg/video.mjpg";
     }
-
     function setCurrentChoice2(result) {
         document.querySelector("#URL2").value = result.URL2 || "http://140.160.167.207:88/CGIProxy.fcgi?cmd=snapPicture2&usr=wl-guest&pwd=123&t=1550389648144";
     }
-
     function setCurrentChoice3(result){
         document.querySelector("#URL3").value = result.URL3 || "http://199.48.198.27/axis-cgi/mjpg/video.cgi?resolution=800x450&dummy=1447007499299";
     }
-
     function setCurrentChoice4(result){
         document.querySelector("#URL4").value = result.URL4 || "http://coupevilleweather.us/webcam/weathercam.jpg";
     }
-
     function setCurrentChoice5(result){
         document.querySelector("#URL5").value = result.URL5 || "http://wwc.instacam.com/instacamimg/SALTY/SALTY_s.jpg";
     }
-
-
-    function onError(error) {
-      console.log(`Error: ${error}`);
-    }
   
-    var getting1 = browser.storage.sync.get("URL1");
-    getting1.then(setCurrentChoice1, onError);
+    var getting = browser.storage.sync.get("URL1");
+    getting.then(setCurrentChoice1);
+    getting = browser.storage.sync.get("URL2");
+    getting.then(setCurrentChoice2);
+    getting = browser.storage.sync.get("URL3");
+    getting.then(setCurrentChoice3);
+    getting = browser.storage.sync.get("URL4");
+    getting.then(setCurrentChoice4);
+    getting = browser.storage.sync.get("URL5");
+    getting.then(setCurrentChoice5);
 
-    var getting2 = browser.storage.sync.get("URL2");
-    getting2.then(setCurrentChoice2, onError);
-
-    var getting3 = browser.storage.sync.get("URL3");
-    getting3.then(setCurrentChoice3, onError);
-
-    var getting4 = browser.storage.sync.get("URL4");
-    getting4.then(setCurrentChoice4, onError);
-
-    var getting5 = browser.storage.sync.get("URL5");
-    getting5.then(setCurrentChoice5, onError);
-    
   }
   
   document.addEventListener("DOMContentLoaded", restoreOptions);
--- popup/watcher.js
+++ popup/watcher.js
@@ -1,44 +1,36 @@
-function onError(error) {
-    console.log(`Error: ${error}`);
-  }
-
   function onGot1(item) {
     if (item.URL1) {
       document.getElementById("URL1").src = item.URL1;
     }
   }
-    var getting1 = browser.storage.sync.get("URL1");
-    getting1.then(onGot1, onError);
-
   function onGot2(item) {
     if (item.URL2) {
       document.getElementById("URL2").src = item.URL2;
     }
   }
-    var getting2 = browser.storage.sync.get("URL2");
-    getting2.then(onGot2, onError);
-
   function onGot3(item) {
     if (item.URL3) {
       document.getElementById("URL3").src = item.URL3;
     }
   }
-    var getting3 = browser.storage.sync.get("URL3");
-    getting3.then(onGot3, onError);
-
   function onGot4(item) {
     if (item.URL4) {
       document.getElementById("URL4").src = item.URL4;
     }
   }
-    var getting4 = browser.storage.sync.get("URL4");
-    getting4.then(onGot4, onError);
-
   function onGot5(item) {
     if (item.URL5) {
       document.getElementById("URL5").src = item.URL5;
     }
   }
-    var getting5 = browser.storage.sync.get("URL5");
-    getting5.then(onGot5, onError);
 
+    var getting = browser.storage.sync.get("URL1");
+    getting.then(onGot1);
+    getting = browser.storage.sync.get("URL2");
+    getting.then(onGot2);
+    getting = browser.storage.sync.get("URL3");
+    getting.then(onGot3);
+    getting = browser.storage.sync.get("URL4");
+    getting.then(onGot4);
+    getting = browser.storage.sync.get("URL5");
+    getting.then(onGot5);
